e0da23798e4b04e2d26477689adf0f9a0271413b
Alan Caruso <alan.carusoo@gmail.com>
2022-12-15 12:18:59 -0300

    todo app local storage update

--- pages/todo-app/main.js
+++ pages/todo-app/main.js
@@ -1,21 +1,24 @@
 const list = document.querySelector("#task-board")
 const button = document.querySelector(".add-item");
 const input = document.querySelector(".add-item-input");
+const deleteAll = document.querySelector("#clear");
 
 const check = 'fa-check-square';
 const checkColor = 'check-color'
 const uncheck = 'fa-square';
 const lineTrough = 'line-trough';
-let id = 0;
+let id;
+let LIST;
 
 function addTask(task, id, done, deleted) {
     if (deleted) { return }
     const DONE = done ? check : uncheck;
     const LINE = done ? lineTrough : '';
+    const COLOR = done ? checkColor : ''
 
     const element = `
         <li class="task">
-            <i class="checkbox fa-regular ${DONE}" data="done" id="${id}"></i>
+            <i class="checkbox fa-regular ${DONE} ${COLOR}" data="done" id="${id}"></i>
             <p class="text ${LINE}">${task}</p>
             <i class="far fa-trash-alt delete-button" data="deleted" id="${id}"></i>
         </li>
@@ -29,19 +32,27 @@ function taskDone(element) {
     element.classList.toggle(uncheck)
     element.classList.toggle(checkColor)
     element.parentNode.querySelector('.text').classList.toggle(lineTrough);
+    LIST[element.id].done = LIST[element.id].done ? false : true
 
 }
 function taskDeleted(element) {
-
     element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].deleted = true
 }
 
 button.addEventListener("click", () => {
     const task = input.value
-    console.log(task)
+
     if (task) {
         addTask(task, id, false, false)
+        LIST.push({
+            name: task,
+            id: id,
+            done: false,
+            deleted: false,
+        })
     }
+    localStorage.setItem('TODO', JSON.stringify(LIST))
     input.value = ''
     id++
 })
@@ -52,10 +63,18 @@ document.addEventListener('keyup', function (event) {
 
         if (task) {
             addTask(task, id, false, false)
+            LIST.push({
+                name: task,
+                id: id,
+                done: false,
+                deleted: false,
+            })
         }
+        localStorage.setItem('TODO', JSON.stringify(LIST))
         input.value = ''
         id++
     }
+
 })
 
 list.addEventListener('click', function (event) {
@@ -67,5 +86,35 @@ list.addEventListener('click', function (event) {
         taskDeleted(element)
     }
 
+    localStorage.setItem('TODO', JSON.stringify(LIST))
+})
+
+
+deleteAll.addEventListener('click', function () {
+    LIST = []
+    id = 0
+    localStorage.setItem('TODO', JSON.stringify(LIST))
+
+    list.innerHTML = ""
 
 })
+
+let data = localStorage.getItem('TODO')
+
+if (data) {
+    LIST = JSON.parse(data)
+    console.log(LIST)
+    id = LIST.length
+    loadList(LIST)
+
+} else {
+    LIST = []
+    id = 0
+    console.log(LIST)
+}
+
+function loadList(data) {
+    data.forEach(function (i) {
+        addTask(i.name, i.id, i.done, i.deleted)
+    });
+}
