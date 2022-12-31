36bcaae19ea4e2eec91c49484d1cb0f08707bb3e
Alan Caruso <alan.carusoo@gmail.com>
2022-12-31 19:50:59 -0300

    dynamic content update

---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+const items = {
+    0: {
+        title: 'To Do App',
+        url: 'pages/todo-app',
+        img: 'img/todo-app.png',
+    },
+    1: {
+        title: 'Experience Rating',
+        url: 'pages/rating-component',
+        img: 'img/rating-component.png',
+    },
+    2: {
+        title: 'QR Code Component',
+        url: 'pages/qr-code-component-main',
+        img: 'img/qr-code.png',
+    },
+    3: {
+        title: 'Personal Card Page',
+        url: 'pages/personal-component-card',
+        img: 'img/personal-card.png',
+    },
+    4: {
+        title: 'Soft UI Design System',
+        url: 'https://www.figma.com/file/gHHRAF9Sn1BJ0OUIJdTJnZ/Soft-UI-Components?node-id=1%3A34',
+        img: 'img/design-system.png',
+    },
+
+}
+const createGallery = () => {
+    const content = document.querySelector(".content");
+    content.innerHTML = Object.keys(items).map(key => {
+        return `<a target="_blank" href="${items[key].url}">
+            <div class="card">
+                <h4>${items[key].title}</h4>
+                <img src="${items[key].img}" alt="">
+            </div>
+        </a>`
+    }).join('');
+
+}
+
+
+
+
+createGallery()
