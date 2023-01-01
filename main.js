4a37594b92a9a54644271eb6fdcd0111a1cfc7fd
Alan Caruso <alan.carusoo@gmail.com>
2023-01-01 08:33:20 -0300

    adventurer ecommerce added

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 
 
 const items = {
+
     0: {
         title: 'To Do App',
         url: 'pages/todo-app',
         img: 'img/todo-app.png',
     },
+
     1: {
         title: 'Experience Rating',
         url: 'pages/rating-component',
         img: 'img/rating-component.png',
     },
     2: {
+        title: 'Adventurer Ecommerce',
+        url: 'pages/adventurer-ecommerce',
+        img: 'img/adventurer-ecommerce.png',
+    },
+    3: {
         title: 'QR Code Component',
         url: 'pages/qr-code-component-main',
         img: 'img/qr-code.png',
     },
-    3: {
+    4: {
         title: 'Personal Card Page',
         url: 'pages/personal-component-card',
         img: 'img/personal-card.png',
     },
-    4: {
+    5: {
         title: 'Soft UI Design System',
         url: 'https://www.figma.com/file/gHHRAF9Sn1BJ0OUIJdTJnZ/Soft-UI-Components?node-id=1%3A34',
         img: 'img/design-system.png',
     },
+
 
 }
 const createGallery = () => {
