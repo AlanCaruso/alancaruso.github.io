7421284ab539d770da462c3dcbe03e939a45945e
Alan Caruso <alan.carusoo@gmail.com>
2023-02-21 23:10:04 -0300

    blog site added

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 
 
 const items = {
-
     0: {
+        title: 'Github Users Searcher',
+        url: 'pages/search-github-users',
+        img: 'img/search-github-users.png',
+    },
+    1: {
         title: 'To Do App',
         url: 'pages/todo-app',
         img: 'img/todo-app.png',
     },
 
-    1: {
+    2: {
         title: 'Experience Rating',
         url: 'pages/rating-component',
         img: 'img/rating-component.png',
     },
-    2: {
+    3: {
         title: 'Adventurer Ecommerce',
         url: 'pages/adventurer-ecommerce',
         img: 'img/adventurer-ecommerce.png',
     },
-    3: {
+    4: {
         title: 'QR Code Component',
         url: 'pages/qr-code-component-main',
         img: 'img/qr-code.png',
     },
-    4: {
+    5: {
         title: 'Personal Card Page',
         url: 'pages/personal-component-card',
         img: 'img/personal-card.png',
     },
-    5: {
+    6: {
         title: 'Soft UI Design System',
         url: 'https://www.figma.com/file/gHHRAF9Sn1BJ0OUIJdTJnZ/Soft-UI-Components?node-id=1%3A34',
         img: 'img/design-system.png',
