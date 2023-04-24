b37a7e3a5b8ef964a552fe8bfaa2b9a2022b75aa
Alan Caruso <alan.carusoo@gmail.com>
2023-04-24 01:56:20 -0300

    adding qr-code-generator-component

--- main.js
+++ main.js
@@ -2,37 +2,42 @@
 
 const items = {
     0: {
+        title: 'QR Code Generator',
+        url: 'pages/qr-code-generator',
+        img: 'img/qr-code-generator.png',
+    },
+    1: {
         title: 'Github Users Searcher',
         url: 'pages/search-github-users',
         img: 'img/search-github-users.png',
     },
-    1: {
+    2: {
         title: 'To Do App',
         url: 'pages/todo-app',
         img: 'img/todo-app.png',
     },
 
-    2: {
+    3: {
         title: 'Experience Rating',
         url: 'pages/rating-component',
         img: 'img/rating-component.png',
     },
-    3: {
+    4: {
         title: 'Adventurer Ecommerce',
         url: 'pages/adventurer-ecommerce',
         img: 'img/adventurer-ecommerce.png',
     },
-    4: {
+    5: {
         title: 'QR Code Component',
         url: 'pages/qr-code-component-main',
         img: 'img/qr-code.png',
     },
-    5: {
+    6: {
         title: 'Personal Card Page',
         url: 'pages/personal-component-card',
         img: 'img/personal-card.png',
     },
-    6: {
+    7: {
         title: 'Soft UI Design System',
         url: 'https://www.figma.com/file/gHHRAF9Sn1BJ0OUIJdTJnZ/Soft-UI-Components?node-id=1%3A34',
         img: 'img/design-system.png',
