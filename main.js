a7e1e22b59104d0163be4b1af91f71c5ab9dba88
Alan Caruso <alan.carusoo@gmail.com>
2023-05-05 14:27:46 -0300

    language update

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
 import items from "./assets/js/portfolio.js";
-import introLanguages from "./assets/js/language.js";
+import introLanguages from "./assets/data/languagesData.js";
 
-window.onload = function () {
-  const createGallery = () => {
-    const content = document.querySelector(".content");
-    content.innerHTML = Object.keys(items)
-      .map((key) => {
-        return `<a target="_blank" href="${items[key].url}">
-              <div class="card">
-                  <h4>${items[key].title}</h4>
-                  <img src="${items[key].img}" alt="">
-              </div>
-          </a>`;
-      })
-      .join("");
-  };
+function createGallery() {
+  const content = document.querySelector(".content");
+  content.innerHTML = Object.keys(items)
+    .map((key) => {
+      return `<a target="_blank" href="${items[key].url}">
+            <div class="card">
+                <h4>${items[key].title}</h4>
+                <img src="${items[key].img}" alt="">
+            </div>
+        </a>`;
+    })
+    .join("");
+}
 
-  createGallery();
-
+document.addEventListener("DOMContentLoaded", function (event) {
   if (window.location.hash) {
     if (window.location.hash === "#es") {
       title.textContent = introLanguages.es.title;
+      subTitle.textContent = introLanguages.es.subTitle;
+      intro.textContent = introLanguages.es.intro;
     }
   }
-};
+
+  const languageButton = document.querySelectorAll(".language-menu a");
+
+  for (let i = 0; i <= languageButton.length; i++) {
+    if (languageButton[i]) {
+      languageButton[i].onclick = function click() {
+        setTimeout(function () {
+          location.reload(true);
+        }, 200);
+      };
+    } else {
+      console.log("language option does not exist");
+    }
+  }
+});
+
+createGallery();
